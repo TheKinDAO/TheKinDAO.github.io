a02720c27d8653003a75d69c42583acea85a559c
AnreThunderson <47703955+AnreThunderson@users.noreply.github.com>
2022-05-04 18:18:14 -0700

    Update ChangeMaker.js

--- ChangeMaker/ChangeMaker.js
+++ ChangeMaker/ChangeMaker.js
@@ -1,43 +1,69 @@
+const container = document.getElementById("container"),
+    option = document.createElement('option'),
+    option1 = document.createElement('option'),
+    Option1 = document.createElement('option'),
+    Option2 = document.createElement('option'),
+    Option3 = document.createElement('option'),
+    Option4 = document.createElement('option');
 var pLayer = 0,
     boxes = [],
-        biN = 0;
-const container = document.getElementById("container");
+    state1 = [],
+    state = [],
+    biN = 0,
+    s = 0,
+    power = 900,
+    contract = 0;
 
-document.addEventListener('mousedown', function() {
+
+// Initialize the board and start the game.
+function init() {
+
+    option.text = "⬛";
+    option1.text = "⬜"
+    Option1.text = "🏠";
+    Option2.text = "🌴";
+    Option3.text = "💧";
+    Option4.text = "🏭";
+
+    for (i = 0; i < 3600; i++) {
+        state1[i] = "";
+        state[i] = "";
+    }
+
+    document.addEventListener('mousedown', function() {
         biN = 1
     });
     document.addEventListener('mouseup', function () {
         biN = 0
     });
 
-// Initialize the board and start the game.
-function init() { 
-    var board = document.createElement('table');
-    var identifier = 1;
-
     function makeRows(rows, cols) {
         container.style.setProperty('--grid-rows', rows);
         container.style.setProperty('--grid-cols', cols);
-        for (c = 0; c < (rows * cols); c++) {
+        for (c = 0; c < (3600); c++) {
             let cell = document.createElement("div");
             container.appendChild(cell).className = "grid-item";
             cell.innerHTML = "";
-            cell.addEventListener('click', set);
+            cell.addEventListener('click', swap);
             cell.addEventListener('mouseover', function() {
-                if (biN == 1) {
+                if (biN == 1 && power != 0) {
                     cell.innerHTML = seLect.value;
+                    power -= 1;
                 }
                 cell.style.border = ".1px solid #C0C0C0";
             });
             cell.addEventListener('mouseout', function () {
                 cell.style.border = "none";
             });
+
             boxes.push(cell);
 
         };
     };
 
-    makeRows(30, 30);
+    makeRows(60, 60);
+
+
 
     startNewGame();
 
@@ -47,15 +73,20 @@ function init() {
 // New game
 function startNewGame() {
     // get from cookies if present
-    var itemsArray = localStorage.getItem('items')
+    itemsArray = localStorage.getItem('items')
     const data = JSON.parse(localStorage.getItem('items'))
 
 
     if (localStorage.getItem('items')) {
         var i = 0;
         boxes.forEach(function (box) {
-
-            box.innerHTML = data[i]
+            if (data[i]) {
+                state[i] = data[i]
+                box.innerHTML = data[i]
+            }
+            else {
+                box.innerHTML = "";
+            }
             i++;
         });
     }
@@ -67,25 +98,24 @@ function startNewGame() {
 }
 
 // Set this box
-function set() {
+function swap() {
     if (this.innerHTML !== "") {
         this.innerHTML = "";
+        power += 1;
+    }
+    else if (power == 0) {
         return;
     }
     else {
         this.innerHTML = seLect.value;
+        power -= 1;
+        myFunk();
     }
-    myFunk();
     pLayer++;
 }
-    function views() {
-        boxes.forEach(function (box) {
-            box.innerHTML = selectbox.value;
-        });
-    }
 
     function save() {
-        var itemsArray = []
+        var itemsArray = [];
         var i = 0;
         boxes.forEach(function (box) {
             itemsArray[i] = box.innerHTML;
@@ -97,8 +127,8 @@ function set() {
 
 
 function myFunk() {
-        let ele = document.getElementById('history');
     let list = "";
+    let ele = document.getElementById('history');
     if (pLayer == 0) {
         list += '<br>Diane placed:' + seLect.value;
     }
@@ -113,7 +143,75 @@ function myFunk() {
         pLayer = -1;
     }
         ele.innerHTML += list;
+}
+
+function set() {
+    var i = 0;
+    let menu2 = document.getElementById('menu2');
+    let menuClone = document.getElementById('menuClone');
+
+    if (s == 0) {
+        s = 1;
+
+        while (seLect.options.length > 0) {
+            seLect.remove(0);
+        }
+
+        seLect.appendChild(option);
+        seLect.appendChild(option1);
+
+        boxes.forEach(function (box) {
+            state[i] = box.innerHTML;
+            box.innerHTML = state1[i];
+            i++;
+
+        });
+    }
+    else if (s == 1) {
+        menuClone.style.visibility = 'visible';
+        menu2.style.visibility = 'hidden';
+        container.style.visibility = 'hidden';
+        container2.style.visibility = 'visible';
+        s = 2;
+        boxes.forEach(function (box) {
+            box.innerHTML = "";
+        });
+
     }
 
+    else if (s == 2) {
+        menuClone.style.visibility = 'hidden';
+        menu2.style.visibility = 'visible';
+        container.style.visibility = 'visible';
+        container2.style.visibility = 'hidden';
+        s = 0;
+                while (seLect.options.length > 0) {
+            seLect.remove(0);
+        }
+
+        seLect.appendChild(Option1);
+        seLect.appendChild(Option2);
+        seLect.appendChild(Option3);
+        seLect.appendChild(Option4);
+
+        boxes.forEach(function (box) {
+            state1[i] = box.innerHTML;
+            box.innerHTML = state[i];
+            i++;
+
+        });
+
+    }
+}
+
+function contract1() {
+    fname.style.visibility = 'hidden';
+    lname.style.visibility = 'visible';
+}
+
+function contract2() {
+    fname.style.visibility = 'visible';
+    lname.style.visibility = 'hidden';
+}
+
     init();
-    
