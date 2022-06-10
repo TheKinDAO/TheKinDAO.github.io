f2870365dcf77cc6c358a53a09601fc0f557492a
AnreThunderson <47703955+AnreThunderson@users.noreply.github.com>
2022-06-10 15:38:17 -0700

    Update ChangeMaker.js

--- ChangeMaker/ChangeMaker.js
+++ ChangeMaker/ChangeMaker.js
@@ -1,31 +1,17 @@
-const container = document.getElementById("container"),
-    option = document.createElement('option'),
-    option1 = document.createElement('option'),
-    Option1 = document.createElement('option'),
-    Option2 = document.createElement('option'),
-    Option3 = document.createElement('option'),
-    Option4 = document.createElement('option');
+const container = document.getElementById("container");
 var pLayer = 0,
     boxes = [],
     state1 = [],
     state = [],
     biN = 0,
-    s = 0,
     power = 900,
-    contract = 0;
+    boo = true;
 
 
 // Initialize the board and start the game.
 function init() {
 
-    option.text = "⬛";
-    option1.text = "⬜"
-    Option1.text = "🏠";
-    Option2.text = "🌴";
-    Option3.text = "💧";
-    Option4.text = "🏭";
-
-    for (i = 0; i < 3600; i++) {
+    for (i = 0; i < 100; i++) {
         state1[i] = "";
         state[i] = "";
     }
@@ -40,62 +26,34 @@ function init() {
     function makeRows(rows, cols) {
         container.style.setProperty('--grid-rows', rows);
         container.style.setProperty('--grid-cols', cols);
-        for (c = 0; c < (3600); c++) {
+        for (c = 0; c < (400); c++) {
             let cell = document.createElement("div");
             container.appendChild(cell).className = "grid-item";
             cell.innerHTML = "";
+            boxes.push(cell);
             cell.addEventListener('click', swap);
             cell.addEventListener('mouseover', function() {
-                if (biN == 1 && power != 0) {
+                if (biN == 1) {
                     cell.innerHTML = seLect.value;
-                    power -= 1;
                 }
-                cell.style.border = ".1px solid #C0C0C0";
+                cell.style.border = "1.5px solid black";
             });
             cell.addEventListener('mouseout', function () {
                 cell.style.border = "none";
             });
-
-            boxes.push(cell);
-
         };
     };
+ 
 
-    makeRows(60, 60);
+    makeRows(20, 20);
 
 
-
-    startNewGame();
 
 }
 
 
 // New game
-function startNewGame() {
-    // get from cookies if present
-    itemsArray = localStorage.getItem('items')
-    const data = JSON.parse(localStorage.getItem('items'))
 
-
-    if (localStorage.getItem('items')) {
-        var i = 0;
-        boxes.forEach(function (box) {
-            if (data[i]) {
-                state[i] = data[i]
-                box.innerHTML = data[i]
-            }
-            else {
-                box.innerHTML = "";
-            }
-            i++;
-        });
-    }
-    else {
-        boxes.forEach(function (box) {
-            box.innerHTML = "";
-        });
-    }
-}
 
 // Set this box
 function swap() {
@@ -113,18 +71,6 @@ function swap() {
     }
     pLayer++;
 }
-
-    function save() {
-        var itemsArray = [];
-        var i = 0;
-        boxes.forEach(function (box) {
-            itemsArray[i] = box.innerHTML;
-            i++;
-        });
-        localStorage.setItem('items', JSON.stringify(itemsArray))
-    }
-
-
 
 function myFunk() {
     let list = "";
@@ -145,73 +91,72 @@ function myFunk() {
         ele.innerHTML += list;
 }
 
-function set() {
-    var i = 0;
-    let menu2 = document.getElementById('menu2');
-    let menuClone = document.getElementById('menuClone');
 
-    if (s == 0) {
-        s = 1;
-
-        while (seLect.options.length > 0) {
-            seLect.remove(0);
-        }
-
-        seLect.appendChild(option);
-        seLect.appendChild(option1);
-
-        boxes.forEach(function (box) {
-            state[i] = box.innerHTML;
-            box.innerHTML = state1[i];
-            i++;
-
-        });
+function imageZoom(imgID, resultID) {
+    var img, lens, result, cx, cy;
+    img = document.getElementById(imgID);
+    result = document.getElementById(resultID);
+    /* Create lens: */
+    lens = document.createElement("DIV");
+    lens.setAttribute("class", "img-zoom-lens");
+    /* Insert lens: */
+    img.parentElement.insertBefore(lens, img);
+    /* Calculate the ratio between result DIV and lens: */
+    cx = result.offsetWidth / lens.offsetWidth;
+    cy = result.offsetHeight / lens.offsetHeight;
+    /* Set background properties for the result DIV */
+    result.style.backgroundImage = "url('" + img.src + "')";
+    result.style.backgroundSize = (img.width * cx) + "px " + (img.height * cy) + "px";
+    /* Execute a function when someone moves the cursor over the image, or the lens: */
+    lens.addEventListener("mousemove", moveLens);
+    img.addEventListener("mousemove", moveLens);
+    /* And also for touch screens: */
+    lens.addEventListener("touchmove", moveLens);
+    img.addEventListener("touchmove", moveLens);
+    function moveLens(e) {
+        var pos, x, y;
+        /* Prevent any other actions that may occur when moving over the image */
+        e.preventDefault();
+        /* Get the cursor's x and y positions: */
+        pos = getCursorPos(e);
+        /* Calculate the position of the lens: */
+        x = pos.x - (lens.offsetWidth / 2);
+        y = pos.y - (lens.offsetHeight / 2);
+        /* Prevent the lens from being positioned outside the image: */
+        if (x > img.width - lens.offsetWidth) { x = img.width - lens.offsetWidth; }
+        if (x < 0) { x = 0; }
+        if (y > img.height - lens.offsetHeight) { y = img.height - lens.offsetHeight; }
+        if (y < 0) { y = 0; }
+        /* Set the position of the lens: */
+        lens.style.left = x + "px";
+        lens.style.top = y + "px";
+        /* Display what the lens "sees": */
+        result.style.backgroundPosition = "-" + (x * cx) + "px -" + (y * cy) + "px";
     }
-    else if (s == 1) {
-        menuClone.style.visibility = 'visible';
-        menu2.style.visibility = 'hidden';
-        container.style.visibility = 'hidden';
-        container2.style.visibility = 'visible';
-        s = 2;
-        boxes.forEach(function (box) {
-            box.innerHTML = "";
-        });
-
+    function getCursorPos(e) {
+        var a, x = 0, y = 0;
+        e = e || window.event;
+        /* Get the x and y positions of the image: */
+        a = img.getBoundingClientRect();
+        /* Calculate the cursor's x and y coordinates, relative to the image: */
+        x = e.pageX - a.left;
+        y = e.pageY - a.top;
+        /* Consider any page scrolling: */
+        x = x - window.pageXOffset;
+        y = y - window.pageYOffset;
+        return { x: x, y: y };
     }
-
-    else if (s == 2) {
-        menuClone.style.visibility = 'hidden';
-        menu2.style.visibility = 'visible';
-        container.style.visibility = 'visible';
-        container2.style.visibility = 'hidden';
-        s = 0;
-                while (seLect.options.length > 0) {
-            seLect.remove(0);
-        }
-
-        seLect.appendChild(Option1);
-        seLect.appendChild(Option2);
-        seLect.appendChild(Option3);
-        seLect.appendChild(Option4);
-
-        boxes.forEach(function (box) {
-            state1[i] = box.innerHTML;
-            box.innerHTML = state[i];
-            i++;
-
-        });
-
+}
+function griD() {
+    if (boo == true) {
+        document.getElementById("myimage").style.zIndex = "-1";
+        boo = false;
+    }
+    else {
+        document.getElementById("myimage").style.zIndex = "1";
+        boo = true;
     }
 }
 
-function contract1() {
-    fname.style.visibility = 'hidden';
-    lname.style.visibility = 'visible';
-}
-
-function contract2() {
-    fname.style.visibility = 'visible';
-    lname.style.visibility = 'hidden';
-}
-
-    init();
+init();
+imageZoom("myimage", "myresult");
